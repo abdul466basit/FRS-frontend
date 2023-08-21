f996ca3a6f0f952b273f1b4f60cb6d0e4d53efdf
abdul466basit <abdulbasitsiddiqui466@gmail.com>
2023-08-21 14:42:39 +0500

    working code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'], // Group React and React DOM into a separate chunk
+          // Define other chunks as needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the warning limit as needed
+  },
+});
